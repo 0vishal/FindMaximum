20e04de72f691902f5540531c4516864ec606ffd
0vishal <vishalsalaskar72@gmail.com>
2021-03-13 02:44:15 +0530

    Moreparameter

--- src/test/java/Generic/GenericTest.java
+++ src/test/java/Generic/GenericTest.java
@@ -5,56 +5,22 @@ import org.junit.jupiter.api.Assertions;
 
     public class GenericTest {
 
-    @Test
-    public void GetMaximum() {
-        Generic max= new Generic();
-        String value = max.Maximum("Apple","Peach","Banana");
-        Assertions.assertEquals("Apple",value);
-    }
-    @Test
+        @Test
         public void GetMaximum() {
         Generic max= new Generic();
-        String value = max.Maximum("Apple","Peach","Banana");
-            Assertions.assertEquals("Peach",value);
+        String value = max.Maximum("Apple","Peach","Banana","Grape","Jackfruit");
+        Assertions.assertEquals("Grape", value);
         }
         @Test
         public void GetMaximum() {
             Generic max= new Generic();
-            String value = max.Maximum("Apple","Peach","Banana");
-            Assertions.assertEquals("Banana",value);
+            int value = (int) max.Maximum(20,30,10,40,50,60);
+            Assertions.assertEquals(60,value);
         }
-        public void GetMaximum() {
-            Generic max= new Generic();
-            int value = (int) max.Maximum(30,20,10);
-            Assertions.assertEquals(30, value);
-        }
+
         @Test
         public void GetMaximum() {
             Generic max= new Generic();
-            int value = (int) max.Maximum(20,30,10);
-            Assertions.assertEquals(30, value);
-        }
-        @Test
-        public void GetMaximum() {
-            Generic max= new Generic();
-            int value = (int) max.Maximum(10,20,30);
-            Assertions.assertEquals(30, value);
-        }
-        @Test
-        public void GetMaximum() {
-            Generic max= new Generic();
-            Double value = (Double) max.Maximum(30.0,20.0,10.0);
-            Assertions.assertEquals(30.0, value);
-        }
-        @Test
-        public void GetMaximum() {
-            Generic max= new Generic();
-            Double value = (Double) max.Maximum(20.0,30.0,10.0);
-            Assertions.assertEquals(30.0, value);
-        }
-        @Test
-        public void GetMaximum() {
-            Generic max= new Generic();
-            Double value = (Double) max.Maximum(10.0,20.0,30.0);
-            Assertions.assertEquals(30, value);
+            Double value = (Double) max.Maximum(30.0,20.0,10.0,40.0,60.,70.0);
+            Assertions.assertEquals(70.0,value);
         }
