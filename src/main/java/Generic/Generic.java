38cfdf5c4e9811fc65dc8bbd4d88cdbd017ec566
0vishal <vishalsalaskar72@gmail.com>
2021-03-13 01:49:15 +0530

    Maxint

--- src/main/java/Generic/Generic.java
+++ src/main/java/Generic/Generic.java
@@ -3,16 +3,15 @@ package Generic;
 import java.util.Arrays;
 
 
-    public class Generic <T extends Comparable<T>> {
+    public class Generic {
 
-        T[] items;
 
-        public Generic() {
-            this.items= items;
-        }
-
-        public <T extends Comparable<T>> T Maximum() {
-            Arrays.sort(items);
-            return (T) items[items.length - 1];
+        public int Maximum(Integer N1, Integer N2, Integer N3 ) {
+            if (N1.compareTo(N2) > 0 && N1.compareTo(N3) > 0)
+                return N1;
+            else if (N2.compareTo(N3) > 0 && N2.compareTo(N1) > 0)
+                return N2;
+            else
+                return N3;
         }
     }
