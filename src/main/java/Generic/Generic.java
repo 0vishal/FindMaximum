18cdd8adaa575dd593514e484ba396f93364c790
0vishal <vishalsalaskar72@gmail.com>
2021-03-13 02:38:55 +0530

    generic class

--- src/main/java/Generic/Generic.java
+++ src/main/java/Generic/Generic.java
@@ -1,8 +1,21 @@
 package Generic;
 
 
-    public class Generic {
+    public class Generic <D extends Comparable<D>> {
 
+        D A, B, C;
+
+        private Generic(D A, D B, D C) {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+
+        public D getmaximum() {
+            return Generic.Maximum(A, B, C);
+        }
+
+    }
 
         public String <D extends comparable <D>> D Maximum (D A, D B, D C ) {
             if (A.compareTo(B) > 0 && A.compareTo(C) > 0)
@@ -12,6 +25,5 @@ package Generic;
             else
                 return C;
         }
-
 
     }
